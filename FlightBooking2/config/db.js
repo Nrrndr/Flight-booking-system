61eab662159dfd4f7c2b8e90daf4c0d202c4fc79
MhfZuu <muhammadhanifzuhair2005@mail.ugm.ac.id>
2025-05-25 07:50:39 +0700

    Refactoring Code

---
+++ FlightBooking2/config/db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+const util = require('util');
+require('dotenv').config();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to database');
+});
+
+db.queryAsync = util.promisify(db.query).bind(db);
+
+module.exports = db;
